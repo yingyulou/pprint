2afe6dad3aa16909346a29e6871127451142cef4
yingyulou <yingyulou@vip.qq.com>
2021-09-02 12:41:56 +0800

    Refactor

---
+++ src/pprint.hpp
@@ -0,0 +1,15 @@
+/*
+    pprint.hpp
+    ==========
+        pprint header.
+*/
+
+#include "CategoryTag.hpp"
+#include "CategoryTraits.h"
+#include "CategoryPromotionTraits.h"
+#include "SubCategoryTraits.h"
+#include "PrintTuple.hpp"
+#include "PrintData.hpp"
+#include "Util.hpp"
+#include "Constants.h"
+#include "Print.hpp"
